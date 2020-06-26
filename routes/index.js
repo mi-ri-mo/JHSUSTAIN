4924284487260c7cff5b31f9fac012df3f6c1b87
Sagittta <js1verj@gmail.com>
2020-06-26 14:42:06 +0900

    배너 글씨 작성, 네비게이션 링크 연결

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'JHSUSTAIN' });
 });
 
+router.get('/Our-Team', function(req, res, next) {
+  res.render('team', { title: 'Our Team' });
+});
+
+router.get('/Our-Services', function(req, res, next) {
+  res.render('services', { title: 'Our Services' });
+});
+
+router.get('/Past-Ongoing-Projects', function(req, res, next) {
+  res.render('projects', { title: 'Past & Ongoing Projects' });
+});
+
+router.get('/In-the-News', function(req, res, next) {
+  res.render('news', { title: 'In the News' });
+});
+
+router.get('/Work-with-Us', function(req, res, next) {
+  res.render('team', { title: 'Work with Us' });
+});
+
 module.exports = router;
