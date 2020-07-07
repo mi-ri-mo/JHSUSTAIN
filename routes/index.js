57a37be2e33ae150c796524866bbaf538c52a1b4
Sagittta <js1verj@gmail.com>
2020-07-07 18:55:56 +0900

    네비게이션 1차 완성

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql_odbc = require('../database/db_conn')();
+var connection = mysql_odbc.init();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
@@ -11,7 +14,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/Our-Team', function(req, res, next) {
-  res.render('team', { title: 'Our Team', banner: '/images/banner/TEAM_BANNER.png' });
+  let memberList = {
+    1: [],
+    2: [],
+    3: [],
+    4: []
+  };
+  let projectList;
+
+  let query = "select member_id, member_name, member_job, member_email, member_image from member;";
+  connection.query(query, (err, result) => {
+      if (err) {
+        return res.send(err);
+      }
+      for (let i = 0; i < result.length; i++) {
+        memberList[result[i].member_id].push(result[i]);
+        query = "select m_project_title, m_project_start, m_project_end from member_project where member_id = ?";
+      }
+      console.log(memberList);
+  });
+  res.render('team', {
+    title: 'Our Team',
+    banner: '/images/banner/TEAM_BANNER.png'
+  });
 });
 
 router.get('/Our-Services', function(req, res, next) {
